8d7f0f8dd9772a3c06418fa714fb40df9241333e
Guido Simón Perre <perreguido@gmail.com>
2025-12-28 00:16:23 -0300

    [IMPROVE] Add Missing Builds

--- dist/firebase/FirebaseApplication.d.ts
+++ dist/firebase/FirebaseApplication.d.ts
@@ -1,5 +1,5 @@
 import { FirebaseApp } from "firebase/app";
-import { Auth } from "firebase/auth";
+import { Auth, User } from "firebase/auth";
 import { Analytics } from "firebase/analytics";
 import { FirebaseStorage } from "firebase/storage";
 export default class FirebaseApplication {
@@ -16,5 +16,11 @@ export default class FirebaseApplication {
     static getAuth(): Auth;
     static getAnalytics(): Analytics | null;
     static getStorage(): FirebaseStorage;
+    static uploadFile(path: string, data: Blob, metadata?: object): Promise<string>;
+    static updateUserProfile(user: User, profile: {
+        displayName?: string | null;
+        photoURL?: string | null;
+    }): Promise<void>;
+    static signOut(): Promise<void>;
 }
 //# sourceMappingURL=FirebaseApplication.d.ts.map
