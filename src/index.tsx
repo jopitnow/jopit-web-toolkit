267d349453e563e8c97da5dfa0324ce42945615d
Guido Simón Perre <perreguido@gmail.com>
2025-12-27 23:56:25 -0300

    [IMPROVE] Add External Firebase Functions

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,7 @@ export { default as Observable } from "./observer/Observable";
 export { default as Observer } from "./observer/Observer";
 
 export { isArray, isNotEmpty, isNullOrEmpty, isValidString, isValidNumber, isValidBoolean } from "./types/TypeChecker";
+
+export { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
+
+export { signOut, updateProfile } from "firebase/auth";
